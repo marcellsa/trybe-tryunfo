9cc4b27bc6a3e7b2fd0bc45d8412aff7e9bb9f96
Marcel Albuquerque <msalbuquerque@hotmail.com>
2022-08-23 18:48:55 -0300

    Requisito 4 implementado.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,28 @@ import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends React.Component {
+  state = {
+    cardName: '',
+    description: '',
+    image: '',
+    atribute1: '0',
+    atribute2: '0',
+    atribute3: '0',
+    rarity: 'normal',
+    superTrunfo: false,
+
+  };
+
+  handleInputChange = (event) => {
+    const { name, value, type, checked } = event.target;
+    this.setState({
+      [name]: (type === 'checkbox' ? checked : value),
+    });
+  };
+
   render() {
+    const { cardName, description, image, atribute1,
+      atribute2, atribute3, rarity, superTrunfo } = this.state;
     return (
       <div>
         <header>
@@ -14,8 +35,28 @@ class App extends React.Component {
 
         <main>
 
-          <Form />
-          <Card />
+          <Form
+            cardName={ cardName }
+            cardDescription={ description }
+            cardImage={ image }
+            cardAttr1={ atribute1 }
+            cardAttr2={ atribute2 }
+            cardAttr3={ atribute3 }
+            cardRare={ rarity }
+            cardTrunfo={ superTrunfo }
+            onInputChange={ this.handleInputChange }
+          />
+          <Card
+            cardName={ cardName }
+            cardDescription={ description }
+            cardImage={ image }
+            cardAttr1={ atribute1 }
+            cardAttr2={ atribute2 }
+            cardAttr3={ atribute3 }
+            cardRare={ rarity }
+            cardTrunfo={ superTrunfo }
+            onInputChange={ this.handleInputChange }
+          />
 
         </main>
 
